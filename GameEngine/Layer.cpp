b702190caa2a6d3c85b721a5a9d4f4a4cba21d60
dounor <open@donoprograms.se>
2015-01-06 16:50:44 +0100

    Slutade vara en idiot
    Förflyttade Layer borttagningsdelen från "render" där den absolut inte
    skall vara till destruktorn där den absolut skall vara...

--- GameEngine/Layer.cpp
+++ GameEngine/Layer.cpp
@@ -13,7 +13,18 @@ Layer::Layer(Layer* colLayer)
 // Ta bort alla sprites som ligger i vårt lager
 Layer::~Layer() 
 {
+	// Remove the layers
+	while (!collisionLayers.empty()) {
+		delete collisionLayers.back();
+		collisionLayers.pop_back();
+	}
 
+	// Delete all sprites associated with this layer
+	while (!physSprites.empty())
+	{
+		delete physSprites.back();
+		physSprites.pop_back();
+	}
 }
 
 void Layer::addPhysSprite(PhysicalSprite* sprite)
@@ -35,16 +46,4 @@ void Layer::update(int delta)
 // Rita samtliga objekt i lagret
 void Layer::render(SDL_Renderer* renderer)
 {
-	// Remove the layers
-	while (!collisionLayers.empty()) {
-		delete collisionLayers.back();
-		collisionLayers.pop_back();
-	}
-
-	// Delete all sprites associated with this layer
-	while (!physSprites.empty())
-	{
-		delete physSprites.back();
-		physSprites.pop_back();
-	}
 }
